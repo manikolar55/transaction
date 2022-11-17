5a9b253c7257b7ed9026b890566a20587bec5cd8
manikolar55 <82044523+manikolar55@users.noreply.github.com>
2022-11-17 19:24:39 +0500

    Update settings.py

--- tracking_fyp/settings.py
+++ tracking_fyp/settings.py
@@ -93,6 +93,11 @@ WSGI_APPLICATION = 'tracking_fyp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd96dl8jcrt3e69',
+        'USER': 'hyhgwtnamxcyxd',
+        'PASSWORD': '5b915d67cf4b342749bdb3feaa04ae79a8e047fc2143613fde6e8071fce7264a',
+        'HOST': 'ec2-54-86-214-124.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
